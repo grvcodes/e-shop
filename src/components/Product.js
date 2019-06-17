1a509d232803310f5d59634ed2bdfeff447bd787
grvcodes <gurv0002@gmail.com>
2019-06-17 14:48:29 +0530

    changed grid layout of product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import addToCart from './icons/svg/add_to_cart.svg'
 import incart from './icons/svg/incart.svg'
+import dollar from './icons/svg/dollar.svg'
+
 class Product extends Component {
     constructor(props){
         super(props);
@@ -12,7 +14,12 @@ class Product extends Component {
                     <img src = {`./${this.props.details.img}`} alt = "product"/><br />
                     <h3 className="specials">{this.props.details.title}</h3>  <h2>by {this.props.details.company}</h2>
                     <br />
-                    <strong>${this.props.details.price}</strong>
+                    <strong>
+                        <span>
+                        <img className="currency" src={dollar}></img>
+                         {this.props.details.price}
+                         </span>
+                    </strong>
                 </section>
                 <p>{this.props.details.info}</p>
                 <div className="product-bottom">
